f7a8308042f790cc8636cf8202b80db23481914d
rodrigogarlopz <rodrigogarlopz2003@gmail.com>
2024-01-15 15:42:26 -0600

    Inicio

---
+++ Game for shool/Assets/Codigos/LogicaObstaculo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public float veloidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime;   
+    }
+}
